740ad394ae4a86b32c5d6172c2f0ad61c47ae284
knowlenger <znaevas@mail.ru>
2023-03-23 18:04:56 +0600

    feat: v1.1 FastApi

--- src/app.py
+++ src/app.py
@@ -3,10 +3,22 @@ from fastapi import FastAPI
 from src.api.base_router import router
 
 
+tags_dict = [
+    {
+        'name': 'authorization',
+        'description': 'Авторизация',
+    },
+    {
+        'name': 'methods',
+        'description': 'Методы для работы с ML',
+    }
+]
+
 app = FastAPI(
     title="Знаев Алексей. FinalGazprom",
     description="Финальный проект",
     version="0.0.1",
+    openapi_tags=tags_dict,
 )
 
 app.include_router(router)
--- src/services/method.py
+++ src/services/method.py
@@ -1,4 +1,3 @@
-import numpy as np
 from fastapi import Depends
 from typing import BinaryIO
 from datetime import datetime
@@ -6,17 +5,9 @@ from src.db.db import Session, get_session
 from src.models.method import Method
 from src.models.schemas.utils.method_enum import Methods_Enum
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import LabelEncoder
 import pickle
-import csv
 import pandas as pd
-from io import StringIO
-
-
-def encode_and_bind(original_dataframe, feature_to_encode):
-    dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
-    res = pd.concat([original_dataframe, dummies], axis=1)
-    res = res.drop([feature_to_encode], axis=1)
-    return res
 
 
 class MethodsService:
@@ -31,6 +22,8 @@ class MethodsService:
         self.session.add(method)
         self.session.commit()
         df = pd.read_csv(input_file)
+        with open('MLmodel/data.csv', 'wb') as fid:
+            pickle.dump(df.to_csv(), fid)
         for i in df.columns:
             if df.dtypes[i] == "float64":
                 df[i] = df[i].fillna(df[i].mean())
@@ -38,10 +31,10 @@ class MethodsService:
                 df[i] = df[i].fillna(df[i].median())
             else:
                 df[i] = df[i].fillna(df[i].mode())
-        df_onehot = df.copy()
+        df_labeled = df.copy()
         for i in ['WeekStatus', 'Day_of_week', 'Load_Type']:
-            df_onehot = encode_and_bind(df_onehot, i)
-        return df_onehot.to_csv(index=False)
+            df_labeled[i] = LabelEncoder().fit_transform(df_labeled[i])
+        return df_labeled.to_csv(index=False)
 
     def fit(self, user_id: int, input_file: BinaryIO):
         method = Method()
@@ -56,8 +49,6 @@ class MethodsService:
         model = DecisionTreeRegressor().fit(X_train, y_train)
         with open('MLmodel/tree_classifier.pkl', 'wb') as fid:
             pickle.dump(model, fid)
-        with open('MLmodel/train_data.csv', 'wb') as fid:
-            pickle.dump(df.to_csv(), fid)
 
     def predict(self, user_id: int, input_file: BinaryIO):
         method = Method()
@@ -66,12 +57,13 @@ class MethodsService:
         method.user_id = user_id
         self.session.add(method)
         self.session.commit()
+        print(input_file)
         df = pd.read_csv(input_file)
         X = df.drop(["Usage_kWh", "date"], axis=1)
         with open('MLmodel/tree_classifier.pkl', 'rb') as fid:
             model = pickle.load(fid)
-            X["Usage_kWh"] = model.predict(X)
-            return X.to_csv()
+            df["Usage_kWh"] = model.predict(X)
+            return df.to_csv()
 
     def download(self, user_id: int):
         method = Method()
@@ -80,6 +72,6 @@ class MethodsService:
         method.user_id = user_id
         self.session.add(method)
         self.session.commit()
-        with open('MLmodel/train_data.csv', 'rb') as fid:
-            train_data = pickle.load(fid)
-            return train_data
+        with open('MLmodel/data.csv', 'rb') as fid:
+            data = pickle.load(fid)
+            return data
